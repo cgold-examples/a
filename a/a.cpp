6c5af5c87a4e7ad03d3ea397b09c9e3a38e26e11
Ruslan Baratov <ruslan_baratov@yahoo.com>
2018-02-05 10:56:19 +0300

    CMake code

---
+++ a/a.cpp
@@ -0,0 +1,12 @@
+#include <a/a.hpp>
+
+#include <x/x.hpp>
+
+namespace a {
+
+void a::Run() {
+  x::x var;
+  var.Run();
+}
+
+} // namespace a
---
+++ a/a.hpp
@@ -0,0 +1,13 @@
+#ifndef A_A_HPP_
+#define A_A_HPP_
+
+namespace a {
+
+class a {
+ public:
+  void Run();
+};
+
+} // namespace a
+
+#endif // A_A_HPP_
